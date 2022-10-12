0c69e8bb14abd1227befc97818a8fb891a619400
Mohamed Abdelmagid <mohamedabdelmagid@Mohameds-MacBook-Air.local>
2022-10-12 14:13:33 -0400

    added styling to header

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
+          alt=""
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
